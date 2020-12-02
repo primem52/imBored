6f8f8d85bf6b32dfa18715c6e6fa5b230a5fc05b
Morgan Prime <primem@bc.edu>
2020-12-01 20:59:24 -0500

    Initial Commit

---
+++ imBored/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  imBored
+//
+//  Created by Morgan Prime on 12/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
